9fd5a445f9cca71809fd9f5068513bed7edc0b7d
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-03-13 21:33:28 +0100

    Smtp setup

--- man.py
+++ man.py
@@ -1,33 +1,41 @@
 #
+import smtplib
+
 import pandas
 import datetime as dt
 import random
-today = (dt.datetime.now().month, dt.datetime.now().day)
 
+my_email = "lesalonstevens@gmail.com"
+password = "btjhrglxzhnjrnuw"
+
+today = (dt.datetime.now().month, dt.datetime.now().day)
 
 data = pandas.read_csv("birthdays.csv")
 
 birthdays = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
 if (today) in birthdays:
- file_path = "letter_"
+    person = birthdays[today]
 
+    file_path = f"letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter:
+        contents = letter.read()
+        contents.replace("[NAME]", person["name"])
 
-
-
-
-
-
-
-
-
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=person["email"],
+                            msg=f"Subject: Happy birthday!\n\n{contents}"
+                            )
 
 #################### Normal Starting Project ######################
 
 # 1. Update the birthdays.csv with your friends & family's details. 
 # HINT: Make sure one of the entries matches today's date for testing purposes. e.g.
-#name,email,year,month,day
-#YourName,your_own@email.com,today_year,today_month,today_day
+# name,email,year,month,day
+# YourName,your_own@email.com,today_year,today_month,today_day
 
 # 2. Check if today matches a birthday in the birthdays.csv
 # HINT 1: Create a tuple from today's month and day using datetime. e.g.
@@ -39,17 +47,17 @@ if (today) in birthdays:
 # birthdays_dict = {
 #     (birthday_month, birthday_day): data_row
 # }
-#Dictionary comprehension template for pandas DataFrame looks like this:
+# Dictionary comprehension template for pandas DataFrame looks like this:
 # new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
-#e.g. if the birthdays.csv looked like this:
+# e.g. if the birthdays.csv looked like this:
 # name,email,year,month,day
 # Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
+# Then the birthdays_dict should look like this:
 # birthdays_dict = {
 #     (12, 24): Angela,angela@email.com,1995,12,24
 # }
 
-#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+# HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
 
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
@@ -62,6 +70,3 @@ if (today) in birthdays:
 # HINT 2: Remember to call .starttls()
 # HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
 # HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
-
-
-
